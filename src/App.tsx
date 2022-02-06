de8ed9fc7192bc26a6631df96fb11ee5b92cc7e1
Mikhail Shestak <stampout386@gmail.com>
2022-02-06 15:12:17 +0300

    lesson 1.3

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,43 @@ import Lesson1 from './lessons/lesson1/lesson1';
 // import './lessons/lesson8/lesson8';
 
 function App() {
+
+    // задача с собеса
+    // let arr = [1, [1, 2, [3, 4]], [2, 4]];
+    // let result: any = [];
+    // const a = (array: any) => {
+    //     for (let i = 0; i < array.length; i++) {
+    //         if (Array.isArray(array[i])) {
+    //             a(array[i])
+    //         } else {
+    //             result.push(array[i]);
+    //         }
+    //     }
+    // }
+    //
+    // const b = (array: any) => {
+    //     let result: any = [];
+    //     array.forEach((item: any) => {
+    //         if (Array.isArray(item)) {
+    //             // @ts-ignore
+    //             result = [...result, ...b(item)]
+    //         } else {
+    //             result = [...result, item]
+    //
+    //         }
+    //     })
+    //     return result
+    // }
+    //
+    // console.log(b(arr))
+    // // a(arr);
+    // // console.log(result)
+
+
     return (
         <div className="container">
             <Lesson1/>
+
             {/*<Lesson3 />*/}
             {/*<Lesson4 />*/}
         </div>
